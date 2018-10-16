18f26a8f5a644d6089848d27c8aca9803a37f8ed
Matthew Migliore <mmigliore311@gmail.com>
2018-10-16 13:56:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(string)
   string_array = string.split(" ")
-  string_array.collect do |word|
+  
+  reverse_array = string_array.collect do |word|
     word.reverse 
   end
   
+  reverse_array.join
 end
